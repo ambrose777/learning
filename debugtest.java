13179d7f8338f9cb715982dafdcff592d60ca979
ambrose777 <54838879+ambrose777@users.noreply.github.com>
2019-09-10 15:51:21 +0800

    Add files via upload

---
+++ debugtest.java
@@ -0,0 +1,15 @@
+package sfdsadf;
+
+public class debugtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius;//Declare radius
+		double area;//Declare area
+		radius=20;//Assign a radius is 20
+		area=radius*radius*3.14159;//Compute area
+		System.out.println("The area for the circle of radius "+radius+ " is " +area);//Display results
+
+	}
+
+}
