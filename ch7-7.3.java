f479e91229de05064f447c698f4155c53a4dd14d
ambrose777 <54838879+ambrose777@users.noreply.github.com>
2019-10-08 14:10:12 +0800

    Update ch7-7.3.java

--- ch7-7.3.java
+++ ch7-7.3.java
@@ -5,11 +5,11 @@ public class ch7 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input= new Scanner(System.in);
-		int[] numbers= new int[100];
-		int total=0;
-		int count;
+		int[] numbers= new int[100];   //储存读入的数据
+		int total=0;                 //总共读入了total个数
+		int count;                   // 用来储存每个数出现的次数
 		System.out.print("Enter the integers between 1 and 100: ");
-		for(int i=0;i<100;i++){
+		for(int i=0;i<100;i++){           //读入数据
 			numbers[i]= input.nextInt();
 			if(numbers[i]==0)
 				break;
@@ -17,12 +17,12 @@ public class ch7 {
 		}
 
 		for(int j=1;j<100;j++){
-			count=0;
+			count=0;            //count归零
 			for(int k=0;k<=total;k++){
 				if(numbers[k]==j)
 					count+=1;
 			}
-			if(count==1)
+			if(count==1)      // 区别复数
 				System.out.println(j + " occurs " + count + " " + "time");
 			else if(count>1)
 				System.out.println(j + " occurs " + count + " " + "times");
