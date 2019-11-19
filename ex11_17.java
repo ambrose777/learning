cdc41503f53fb5b0fa2375b9e1ca81a4ceedf26a
ambrose777 <54838879+ambrose777@users.noreply.github.com>
2019-11-19 15:59:57 +0800

    Add files via upload

---
+++ ex11_17.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class ex11_17 {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+        System.out.print("Enter an integer m:");
+        int m=input.nextInt();
+        int flag=m;
+        int i=2;
+        int n=1;
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        ArrayList<Integer> list1=new ArrayList<Integer>();
+        int []a=new int[1000];
+        while(m!=1) {
+            if(m%i==0) {
+                list.add(i);
+                m=m/i;
+                a[i]++;
+            }
+            else
+                i++;    
+
+        }
+        for(int j=0;j<1000;j++) {
+            if(a[j]!=0&&(a[j]%2==1))
+                n=n*j;
+        }
+        System.out.print("The smallest number n for m * n to be a perfect square is ");
+        System.out.println(n);
+        System.out.print("m * n is ");
+        System.out.println(n*flag);
+	}
+}
